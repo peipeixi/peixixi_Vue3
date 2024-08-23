b484078b1e0e39cfd29bd71e3c5de9aff678f076
zeng121110 <287634162@qq.com>
2024-08-24 01:18:36 +0800

    创建effect函数和ReactiveEffect类

--- packages/reactivity/src/baseHanddle.ts
+++ packages/reactivity/src/baseHanddle.ts
@@ -1,4 +1,5 @@
 import { ReactiveFlags } from "./constants";
+import { reactiveEffect } from "./effect";
 
 export const mutableHandler: ProxyHandler<any> = {
     get(target, key, receiver) {
@@ -6,6 +7,10 @@ export const mutableHandler: ProxyHandler<any> = {
             //判断是否是代理对象，未被代理过的对象读取该key时不会进入get
             return true;
         }
+
+        //依赖收集，当读取属性时，将当前effect收集到该属性的依赖中，将effect和key一一对应
+        console.log(key, reactiveEffect);
+
         /**
          *  这里使用Reflect的原因是？receiver用于调用目标对象的方法时指定方法中 this 的值从而使this指向的属性也能通过代理代理对象来访问
             举例：
@@ -23,7 +28,7 @@ export const mutableHandler: ProxyHandler<any> = {
             })
             console.log(proxy.fullname);
             这个例子中当通过proxy访问fullname时，
-            get方法如果是return target[key]，fullname中的this指向的是obj，而不是proxy,读取name属性就没有通过代理对象
+            get方法如果是return target[key]，fullname中的this指向的是obj，而不是proxy,读取name属性就没有通过代理对象导致更改了name属性的值没有促发更新视图
             get方法如果是return Reflect.get(target,key,receiver)，fullname中的this指向的是receiver，读取name属性也是通过代理对象
         */
         return Reflect.get(target, key, receiver);
--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,1 +1,34 @@
-export function effect(fn, options: any = {}) {}
+export let reactiveEffect; //当前正在执行的effect，用于在依赖收集时,将当前effect添加到依赖中
+/**
+ * ReactiveEffect类,用于注册副作用函数
+ */
+class ReactiveEffect {
+    constructor(public fn, public scheduler) {}
+
+    // 执行副作用函数
+    run() {
+        let lastReactiveEffect = reactiveEffect; //缓存上一次的全局effect,针对effect嵌套的情况
+        try {
+            // 将当前effect赋值给全局变量,用于依赖收集
+            reactiveEffect = this;
+            this.fn();
+        } finally {
+            //reactiveEffect = undefined;// 清空全局变量，防止不需要执行effect时,还引用了effect
+            reactiveEffect = lastReactiveEffect; // 恢复全局effect变量
+        }
+    }
+}
+
+/**
+ * effect函数,用于注册副作用函数,当响应式数据变化时,会重新执行副作用函数
+ * @param fn effect执行的回调函数
+ * @param options effect的配置项
+ */
+export function effect(fn, options: any = {}) {
+    const _effect = new ReactiveEffect(fn, () => {
+        // scheduler函数。当effect执行时,会调用这个函数,这个函数会执行scheduler
+        _effect.run();
+    });
+    _effect.run(); // 默认执行一次
+    return _effect;
+}
